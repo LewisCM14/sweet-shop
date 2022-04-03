c72b270803030d68bdf4119503142a0361f845ee
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-03 17:46:31 +0000

    install dj_database_url & psycopg2-binary, define database in settings.py

--- sweet_shop/settings.py
+++ sweet_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,12 +107,20 @@ WSGI_APPLICATION = 'sweet_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# when the app is running on Heroku, where DATABASE_URL is defined
+# Connect to Postgres, otherwise connect to sqlite light.
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
