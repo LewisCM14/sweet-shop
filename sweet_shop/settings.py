a94da791d7171166296e46dc41df505477d27b15
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-03 17:48:16 +0000

    update and hide SECRET_KEY

--- sweet_shop/settings.py
+++ sweet_shop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3kex&37o$rbj9db3t!23d$!=286!xp*x##231cfe!@dd63klbp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
