94913398205895da61a4b669a4763f9ceb2a4ecd
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-03 19:11:14 +0000

    define DIRS in TEMPLATES within settings.py

--- sweet_shop/settings.py
+++ sweet_shop/settings.py
@@ -69,7 +69,10 @@ ROOT_URLCONF = 'sweet_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
