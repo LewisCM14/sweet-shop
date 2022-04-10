184ad73972c4ce1ed9f47297dc3bd9b33e0f7c6d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-10 21:29:45 +0000

    rename forms.py in profile app to profile_form.py

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
-from .forms import UserProfileForm
+from .profile_form import UserProfileForm
 
 
 # pylint: disable=redefined-outer-name
