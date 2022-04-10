afb0f5b17a3cb03c8586f361ee09e2a9f96a24e4
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-10 19:07:02 +0000

    define create_or_update_user_profile method

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
@@ -32,3 +34,21 @@ class UserProfile(models.Model):
     # pylint: disable=no-member
     def __str__(self):
         return self.user.username
+
+
+# pylint: disable=no-member
+# pylint: disable=unused-argument
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the UserProfile.
+
+    Receiver for the post save event from the user model.
+    Each time a user object is saved, either create a profile for them,
+    if the user has just been created,
+    Or just save the profile to update it if the user already existed.
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,22 @@
 """ This module contains the views for the profiles app """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """
-    Renders the user profile
+    Renders the user profile in the profile.html template.
+
+    Uses get_object_or_404 to fetch the current signed in
+    users profile from the UserProfile model before
+    returning it in the context.
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
