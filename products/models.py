a04c2faf04afb3523c6a42aecdcaf53330da570a
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-12 23:01:01 +0000

    add year field to product model

--- products/models.py
+++ products/models.py
@@ -35,14 +35,19 @@ class Product(models.Model):
     A Model to hold the product info.
 
     category field is a forign key to the Category Model above.
-    Each product requires a name, description and price.
+    Each product requires a name, description, year and price.
     Everything else is optional.
     """
+
+    # A tuple to hold the key for the year field.
+    YEAR = ((0, "90s & 00s"), (1, "90s"), (2, "00s"))
+
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
     )
     name = models.CharField(max_length=254)
     description = models.TextField()
+    year = models.IntegerField(choices=YEAR, default=0)
     has_weights = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
