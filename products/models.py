9bb05b9cf22e8b16c8cf220ff47966489bbc3dc1
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-12 22:31:56 +0000

    create product model

--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
     # pylint: disable=invalid-str-returned
     def __str__(self):
         """
-        Returns the category name as a string.
+        Returns the specific category name as a string.
         """
         return self.name
 
@@ -28,3 +28,29 @@ class Category(models.Model):
         A model method to return the friendly name.
         """
         return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    A Model to hold the product info.
+
+    category field is a forign key to the Category Model above.
+    Each product requires a name, description, weight and price.
+    Everything else is optional.
+    """
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    weight_in_grams = models.IntegerField(max_length=10)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    # pylint: disable=invalid-str-returned
+    def __str__(self):
+        """
+        Returns the specific product name as a string.
+        """
+        return self.name
