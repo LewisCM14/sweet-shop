741928ee6eaf6b2138e2d2163cfed88445af8607
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-12 23:15:34 +0000

    register products models in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+""" This module contains the admin logic for the models products app """
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, Type
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    The admin class for the Products model.
+    extends from the base.
+    """
+    list_display = (
+        'name',
+        'type',
+        'year',
+        'price',
+        'image'
+    )
+
+    list_filter = ('type', 'year',)
+
+    ordering = ('name',)
+
+
+@admin.register(Type)
+class CatergoryAdmin(admin.ModelAdmin):
+    """
+    The admin class for the Type model.
+    extends from the base.
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
