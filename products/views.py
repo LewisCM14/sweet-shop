abd4ec3e548b1ee42f0ef23cc99eff6c4aa53e53
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-14 23:32:48 +0000

    create product_detail view and template

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 """ This module handles the views for the products app """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
