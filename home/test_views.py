651d6c6207d455d0652e9f4197a57a78d338a9f3
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-17 21:55:36 +0000

    create test_get_deliver_info_page

--- home/test_views.py
+++ home/test_views.py
@@ -28,3 +28,13 @@ class TestModel(TestCase):
         """
         response = self.client.get('/faq/')
         self.assertEqual(response.status_code, 200)
+
+    def test_get_deliver_info_page(self):
+        """
+        Tests the delivery information page renders.
+
+        Uses Django's in-built HTTP client to get the delivery info page URL.
+        Asserts equal to status code 200, a successful HTTP response.
+        """
+        response = self.client.get('/delivery-information/')
+        self.assertEqual(response.status_code, 200)
