9f178183231656b160e95597b15beaf597db961d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-20 18:58:32 +0000

    fix type sorting on products.html selector

--- products/views.py
+++ products/views.py
@@ -62,8 +62,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
+            if sortkey == 'type':
+                sortkey = 'type__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
