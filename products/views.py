da51c318cae54f4a7a4a880cf70515cd1d649c22
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-20 18:49:49 +0000

    wire up sort selector on products.html

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
 from .models import Product, Type
 
@@ -58,6 +59,11 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
