ebc2475fc9e4a5cf956849bf106ebcdacf4faad5
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-21 17:17:33 +0000

    create test_can_sort_products_by_name

--- products/test_views.py
+++ products/test_views.py
@@ -159,7 +159,15 @@ class TestView(TestCase):
         response = self.client.get('/products/?year=00')
         self.assertEqual(response.status_code, 200)
 
-    # def test_can_sort_products(self):
-    #     """
-    #     Tests the all_products view sorts objects by given params.
-    #     """
+    def test_can_sort_products_by_name(self):
+        """
+        Tests the all_products view sorts objects by name.
+
+        Uses Django's in-built HTTP client to get the sort URL's.
+        Ensuring a status code 200 is returned. A successful HTTP response.
+        """
+        response = self.client.get('/products/?sort=name&direction=asc')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/?sort=name&direction=desc')
+        self.assertEqual(response.status_code, 200)
