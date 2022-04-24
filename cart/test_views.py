be653a0123814bd6c88e582a5f71c512ba7ce57b
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-24 19:00:34 +0000

    create test_add_to_cart_redirects_to_item

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,6 +1,7 @@
 """ This module tests the cart app views """
 
 from django.test import TestCase
+from django.urls import reverse
 from products.models import Type, Product
 
 
@@ -18,11 +19,6 @@ class TestView(TestCase):
             friendly_name='Sour',
         )
 
-        chewy = Type.objects.create(
-            name='chewy',
-            friendly_name='Chewy'
-        )
-
         Product.objects.create(
             type=sour,
             name='Toxic Waste',
@@ -34,18 +30,7 @@ class TestView(TestCase):
             price='4.99',
         )
 
-        Product.objects.create(
-            type=chewy,
-            name='Bon Bons',
-            description='A chewy Sweet',
-            popular_in_80s=False,
-            popular_in_90s=False,
-            popular_in_00s=True,
-            weight_in_grams='75',
-            price='2.99',
-        )
-
-    def test_get_cart_page(self):
+    def test_cart_view_renders(self):
         """
         Tests the view_cart page renders.
 
@@ -55,3 +40,25 @@ class TestView(TestCase):
 
         response = self.client.get('/cart/')
         self.assertEqual(response.status_code, 200)
+
+    def test_add_to_cart_redirects_to_item(self):
+        """
+        Tests the add_to_cart view redirects back to the product_detail
+        page of the product added.
+
+        Passes the ID for the product created in the setUp method as the
+        argument to the reverse of the add_to_cart url,
+        with a quantity of 1 and redirect_url value for that of the product.
+
+        Uses Django's in-built HTTP client to asserts the status code is
+        equal to 302, a successful HTTP redirect response. Before asserting the
+        redirect url is the correct one for the object added.
+        """
+
+        response = self.client.post(reverse("add_to_cart", args=[1]), {
+            'quantity': '1',
+            'redirect_url': '/products/1/',
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/products/1/')
