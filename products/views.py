f9544aa71c5b7952da73bc5e6f90926bd18058ea
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-28 16:48:18 +0000

    create edit_product template & view

--- products/views.py
+++ products/views.py
@@ -159,3 +159,48 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """
+    A view to allow superusers to edit products in the store.
+
+    Superuser credentails must first be verified.
+
+    Take in the request and the product ID the user is going to edit.
+    Pre-fill the form by getting the product using get_object_or_404
+    And then instantiating a product form using the product.
+
+    If the request method is POST.
+    Submit the form using request.post and request.files.
+    Tell it the specific instance to update is the product obtained above.
+    If the form is valid save it, add a success message
+    and then redirect to the product detail page using the product ID.
+    Otherwise, add an error message and return the form,
+    which will have the errors attached.
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')  # noqa
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
