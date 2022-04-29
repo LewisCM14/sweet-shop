d0f917518413dbcbcefe3091221870d8f439fc44
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-29 23:28:52 +0000

    create test_unregistered_users_cannot_access_edit_product_page

--- products/test_views.py
+++ products/test_views.py
@@ -342,3 +342,20 @@ class TestProductManagement(TestCase):
         self.admin_login()
         response = self.client.get('/products/edit/1/')
         self.assertEqual(response.status_code, 200)
+
+    def test_unregistered_users_cannot_access_edit_product_page(self):
+        """
+        Tests an unregistered site user cannot access the edit products page.
+
+        With no user signed in attempts to access the edit products url for the
+        product created in the setUp method, stored in the response variable.
+
+        Uses Django's in-built HTTP client to assert the status code on
+        the response variable is equal to 302, a successful HTTP redirect.
+        Then asserts this redirect url is the login page.
+        """
+
+        response = self.client.get('/products/edit/1/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/products/edit/1/')  # noqa
