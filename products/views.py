bb879734aad0b17965769f911e33c5f43c11d4bc
LewisCM14 <Lewiscmartin@live.co.uk>
2022-04-29 17:49:14 +0000

    write delete_product view and wire up on templates

--- products/views.py
+++ products/views.py
@@ -204,3 +204,27 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """
+    A view to allow superusers to edit products in the store.
+
+    Superuser credentails must first be verified.
+    Delete a product from the store
+
+    Takes in the request and the product id to be deleted.
+    First collect the product with get_object_or_404,
+    then call product.delete.
+    Add a success message and redirect back to the all products page.
+    """
+    # Allows only superusers access to this views functionality
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
