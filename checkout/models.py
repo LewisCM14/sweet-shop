5d5a43a0a908dbc260c362cf106d29c6eaf79a3d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-05-02 14:16:12 +0000

    render the country field on the order form to match other fields

--- checkout/models.py
+++ checkout/models.py
@@ -55,7 +55,7 @@ class Order (models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label="Country *", null=False, blank=False)
+    country = CountryField(blank_label="Country", null=False, blank=False)
     # Date of order
     date = models.DateTimeField(auto_now_add=True)
     # Calculated using model methods.
