7d51b11db0cc079845eafd535f153f3bee4778e6
LewisCM14 <Lewiscmartin@live.co.uk>
2022-05-07 17:33:18 +0000

    create the webhook to handle stripe payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),  # noqa: E501
+    path('wh/', webhook, name='webhook'),
 ]
