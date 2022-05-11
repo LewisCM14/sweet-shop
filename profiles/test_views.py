df9e56a3b001dec2db14e77b84d2f42c33404740
LewisCM14 <Lewiscmartin@live.co.uk>
2022-05-11 19:08:13 +0000

    create test_order_history_is_collected

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,8 +1,13 @@
 """ This module tests the profile app views """
 
+from decimal import Decimal
+
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+from products.models import Type, Product
+from checkout.models import Order
 from .models import UserProfile
 
 
@@ -18,11 +23,27 @@ class TestView(TestCase):
         Creates a User test case.
         """
         User.objects.create_user(
-            username='johndoe',
+            username='Test User',
             first_name='john',
             last_name='doe',
             email='johndoe@email.com',
             password='password',
+        )
+
+        chewy = Type.objects.create(
+            name='chewy_sweets',
+            friendly_name='Chewy Sweets',
+        )
+
+        Product.objects.create(
+            type=chewy,
+            name='Raspberry Bon Bons',
+            description='A Chewy Sweet',
+            popular_in_80s=False,
+            popular_in_90s=True,
+            popular_in_00s=True,
+            weight_in_grams=200,
+            price=Decimal(1.99),
         )
 
     def login(self):
@@ -36,6 +57,39 @@ class TestView(TestCase):
             email="johndoe@email.com",
             password='password',
         )
+
+    def initiate_cart(self):
+        """
+        A helper method to initiate an instance of the cart object.
+        Then stored within the session, used to then prefrom tests on.
+
+        Then collects the created session,
+        storing it in the 'session' variable. From this variable
+        asserts the 'cart' key has a length of 1.
+        Meaing a cart object has been created and a key:value pair passed.
+
+        From the session variable then collects the cart dict itself,
+        storing it in the cart variable. From here asserts that the
+        value of the key '1' is the integer 1. Meaning the product with an ID
+        of '1' has a quantity of 1 stored within the cart dict.
+
+        These tests are done as it is imperative the method creates a cart
+        object within the session, and the key:value pair in it is pre-defined.
+        """
+
+        product = Product.objects.get(id=1)
+        quantity = 1
+
+        self.client.post(reverse("add_to_cart", args=[product.id]), {
+            'quantity': quantity,
+            'redirect_url': '/products/1/',
+        })
+
+        session = self.client.session
+        self.assertEqual(len(session['cart']), 1)
+
+        cart = session['cart']
+        self.assertEqual(cart.get('1'), quantity)
 
     def test_get_profile_page(self):
         """
@@ -100,3 +154,44 @@ class TestView(TestCase):
         self.assertEqual(user_profile.default_county, 'surrey')
         self.assertEqual(user_profile.default_postcode, 'CR2 5ER')
         self.assertEqual(user_profile.default_country, 'GB')
+
+    def test_order_history_is_collected(self):
+        """
+        A view to test a users order history is collected in the view.
+
+        Using the class helper methods, logs into the test user and
+        initiates a cart, posting this cart instance to the checkout
+        view and then using the created order to post to the
+        checkout_success view in order to attach it to the test users
+        profile.
+
+        This profile is then collected via its ID and the all method is
+        used to collect any orders saved to their profile, this variables
+        is asserted to equal 1, before the order_number is used to post
+        to the order_history URL and Django's inbuilt HTTP client is used
+        to confirm a status code 200 is received. 
+        """
+        self.login()
+        self.initiate_cart()
+        self.client.post(
+            reverse("checkout"), {
+                'full_name': 'John Doe',
+                'email': 'johndoe@email.com',
+                'phone_number': '11111111111',
+                'street_address1': '4 privet drive',
+                'street_address2': '',
+                'town_or_city': 'little whinging',
+                'county': 'surrey',
+                'postcode': 'CR2 5ER',
+                'country': 'GB',
+                'client_secret': 'client secret test string',
+            })
+        order = Order.objects.get(id=1)
+        self.client.post(reverse("checkout_success", args=[order.order_number]))  # noqa: E501
+
+        profile = UserProfile.objects.get(id=1)
+        orders = profile.orders.all()
+        self.assertEqual(len(orders), 1)
+
+        response = self.client.get(reverse('order_history', args=[order.order_number]))  # noqa: E501
+        self.assertEqual(response.status_code, 200)
