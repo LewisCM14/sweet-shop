6f57fa75486916b1a28a95ac3416be9979141347
LewisCM14 <Lewiscmartin@live.co.uk>
2022-05-26 21:08:47 +0000

    render contact details and inquiry form on contact us template

--- inquiry/views.py
+++ inquiry/views.py
@@ -1,8 +1,18 @@
 """ This module handles the views for the inquiry app """
 
 from django.shortcuts import render
+from .contact_form import ContactForm
 
 
 def contact_us(request):
     """ Renders the contact_us.html template in the browser """
-    return render(request, 'inquiry/contact_us.html')
+
+    form = ContactForm()
+
+    template = 'inquiry/contact_us.html'
+
+    context = {
+        'contact_form': form,
+    }
+
+    return render(request, template, context)
